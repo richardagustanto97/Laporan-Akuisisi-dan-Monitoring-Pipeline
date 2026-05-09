42b33c2d89ace0da24840e7617e7951c8293b25e
richardagustanto97 <richardagustanto97@gmail.com>
2026-05-09 19:40:32 +0700

    Update webAppUrl to new endpoint

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const webAppUrl = "https://script.google.com/macros/s/AKfycbxlRtxPV5QIFGoDZT3RK77Y7bxLOMMU0P8JsYAy2paVXBphiJm5I26dtESDZWui_MAN/exec";
+const webAppUrl = "https://script.google.com/macros/s/AKfycby1wqgNZdWjF1h0-twOMYbJsIYPnvoL3DniUjtQou5cw15OCijKgLjjEwSkpW-v7Dy9/exec";
 
 const validCodes = ["11900", "11902", "11903", "11904", "11906", "11907", "11912", "11916", "11920", "11923", "11924", "11929", "11931", "11932", "11934", "11935", "11936", "11937"];
 
